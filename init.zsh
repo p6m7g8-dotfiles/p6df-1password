ff7ca6026e63eac801ab4f8d746933a4b9c3f615
Philip M. Gollucci <pgollucci@p6m7g8.com>
2025-10-11 10:50:53 -0400

    fix: 1passsword -> 1password (#7)

--- init.zsh
+++ init.zsh
@@ -15,14 +15,14 @@ p6df::modules::1password::deps() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::1passsword::external::brew()
+# Function: p6df::modules::1password::external::brew()
 #
 #>
 ######################################################################
-p6df::modules::1passsword::external::brew() {
+p6df::modules::1password::external::brew() {
 
   p6df::modules::homebrew::cli::brew::install 1password-cli
   p6df::modules::homebrew::cli::brew::install 1password --cask
- 
+
   p6_return_void
 }
