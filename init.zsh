1cc7b6370125b3d8b5e37213985484970f7a3dfe
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 13:13:07 -0400

    fix(prompt): use double quotes for variable expansion in p6_return_words (SC2016)

--- init.zsh
+++ init.zsh
@@ -64,7 +64,7 @@ p6df::modules::1password::profile::on() {
 ######################################################################
 p6df::modules::1password::profile::mod() {
 
-  p6_return_words '1password' '$OP_ACCOUNT' '$OP_EMAIL' '$OP_VAULT_NAME'
+  p6_return_words '1password' "$" "$" "$"
 }
 
 ######################################################################
